f8415931cae29c2deec446681364d220eefd9349
Admin <Admin@PHUONG>
2024-07-23 23:28:50 +0700

    Add project files.

---
+++ EntityFrameworkCore/Models/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFrameworkCore.Models
+{
+ 
+    internal class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public SchoolContext() : base("name=SchoolDB") 
+        {
+        }
+    }
+}
---
+++ EntityFrameworkCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using EntityFrameworkCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var std = new Student()
+                {
+                    FirstName = "Bill",
+                    LastName = "Gates"
+                };
+                context.Students.Add(std);
+                context.SaveChanges();
+
+                var studentToUpdate = context.Students.First<Student>();
+                studentToUpdate.FirstName = "Steve";
+                context.SaveChanges();
+
+                var studentToDelete = context.Students.First<Student>();
+                context.Students.Remove(studentToUpdate);
+                context.SaveChanges();
+            }
+        }
+    }
+}
